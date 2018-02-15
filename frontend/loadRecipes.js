564f3bdc45b685d57f0a8e70ce2ef68d3a38989d
rklonner <r.klonner@gmx.at>
2018-02-15 21:25:47 +0100

    remove frontend for angular2

--- frontend/loadRecipes.js
+++
@@ -1,9 +0,0 @@
-$('#getRecipes').on('click', function() {
-  $.getJSON('data/recipes.json', function (data) {
-    console.log(data);
-	var compiledTemplate = Handlebars.getTemplate('tableRecipes');
-	var html = compiledTemplate(data);
-	$('#recipesBox').empty();
-	$(html).appendTo('#recipesBox');
-  });
-});
