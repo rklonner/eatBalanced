a6b03cb977f3064f31d1dff9f0ba187d5992d26d
rklonner <r.klonner@gmx.at>
2018-02-24 12:15:00 +0100

    added moveUp and for recipe sorting in menuplan, switched is eating colors from blue/yellow to green/grey

--- frontend/src/app/menuplan.service.ts
+++ frontend/src/app/menuplan.service.ts
@@ -24,11 +24,34 @@ export class MenuplanService {
 	return this.selectedRecipes[index];
   }
   
-  moveUp(index: number) {
-	//this.selectedRecipes.splice(index+
+  moveRecipeUp(index: number) {
+	// only move recipe if there are at least 2 entries
+	if (this.selectedRecipes.length > 1) {
+      let tmp = this.selectedRecipes.splice(index, 1)[0];
+      if (index - 1 == -1) {
+		// splice(-1, 0, tmp) does not work, puts it on vorletzte stelle, not last
+		this.selectedRecipes.push(tmp);
+	  }
+	  else {
+	    this.selectedRecipes.splice(index - 1, 0, tmp);
+	  }
+	}
   }
-  
+
   moveDown(index: number) {
+	// only move recipe if there are at least 2 entries
+	if (this.selectedRecipes.length > 1) {
+      let tmp = this.selectedRecipes.splice(index, 1)[0];
+      if (index + 1 == this.selectedRecipes.length + 1) {
+		// splice(index+1, 0, tmp) does not work at end beacuse array is not
+		// this length any more because of previous splice,
+		// use unshift to place it at beginning in this case
+		this.selectedRecipes.unshift(tmp);
+	  }
+	  else {
+	    this.selectedRecipes.splice(index + 1, 0, tmp);
+	  }
+	}
   }
   
   delete(index: number) {
